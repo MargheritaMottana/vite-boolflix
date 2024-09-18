c76e50a33bbdd7bfb269ee50632d702cc937ebf0
MargheritaMottana <margherita.mottana@gmail.com>
2024-09-18 23:07:11 +0200

    Aggiunta anche la chiamata api delle serie tv e mostrata in pagina in una nuova sezione

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,12 +9,13 @@
 					searchText: '',
 					APIKey: '796dd0c05d6cbaf28484679c71b661a0',
 					movies: [],
+					series: [],
 				}
 			},
 			methods:{
 				// aggiungo il metodo a cui si appoggia il bottone per la ricerca API
-				searchMovie(){
-					// chiamata con due parametri
+				search(){
+					// chiamata con due parametri per i film
 					axios.get('https://api.themoviedb.org/3/search/movie',{
 						params:{
 							api_key: this.APIKey,
@@ -26,9 +27,21 @@
 						// console.log(resp.data.results)
 						this.movies = resp.data.results;
 					})
-					// svuoto il campo dell'input dopo aver visualizzato i titoli
+
+					// chiamata con due parametri per le serie
+					axios.get('https://api.themoviedb.org/3/search/tv',{
+						params:{
+							api_key: this.APIKey,
+							query: this.searchText,
+						}
+					})
+					.then((resp) => {
+						console.log(resp.data.results)
+						this.series = resp.data.results;
+					})
 					this.searchText = ''
 				},
+
 				// funzione per gestire le bandiere
 				flag(language){
 					// salvo i dati delle lingue che hanno una corrispondenza con le immagini
@@ -77,7 +90,7 @@
 				<input v-model="searchText" type="text" class="form-control" placeholder="Cosa vuoi vedere?">
 
 				<!-- bottone da cui ascolto il click per cercare i film/serie tv -->
-				<button @click="searchMovie()" class="btn btn-outline-secondary" type="submit">
+				<button @click="search()" class="btn btn-outline-secondary" type="submit">
 					<i class="fa-solid fa-magnifying-glass"></i>
 				</button>
 			</div>
@@ -87,9 +100,12 @@
 
 	<main>
 
+		<!-- film -->
 		<div class="container">
 
 			<div class="row">
+
+				<h2>Film</h2>
 
 				<!-- ciclo per ripetere la card con le info dei film -->
 				<div class="col mb-3" v-for="(movie, i) in movies" :key='i'>
@@ -104,6 +120,35 @@
 							<li class="list-group-item">Titolo originale: {{movie.original_title}}</li>
 							<li class="list-group-item">Lingua originale: <img :src="flag(movie.original_language)" alt="" class="imgLanguage"></li>
 							<li class="list-group-item">Voto: {{movie.vote_average}}</li>
+						</ul>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<!-- serie -->
+		<div class="container">
+
+			<div class="row">
+
+				<h2>Serie tv</h2>
+
+				<!-- ciclo per ripetere la card con le info dei film -->
+				<div class="col mb-3" v-for="(serie, i) in series" :key='i'>
+
+					<!-- card in cui compaio le infomrazioni del film -->
+					<div class="card">
+							<img src="..." class="card-img-top" alt="...">
+						<div class="card-body">
+							<h5 class="card-title">Titolo: {{serie.name}}</h5>
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Titolo originale: {{serie.original_name}}</li>
+							<li class="list-group-item">Lingua originale: <img :src="flag(serie.original_language)" alt="" class="imgLanguage"></li>
+							<li class="list-group-item">Voto: {{serie.vote_average}}</li>
 						</ul>
 					</div>
 

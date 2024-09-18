8a32fe8dd361b0183a4213ef47ef5c5556162167
MargheritaMottana <margherita.mottana@gmail.com>
2024-09-18 21:27:52 +0200

    - Aggiunto container e riga per far sì che i film compaiano uno accanto all'altro - aggiunto ciclo per far sì che ogni film compaia in una card - aggiunti text interpolation per scrivere i dati dei film nella card - svuotato l'input una volta che i film compaiono in pagina

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -26,6 +26,8 @@
 						// console.log(resp.data.results)
 						this.movies = resp.data.results;
 					})
+					// svuoto il campo dell'input dopo aver visualizzato i titoli
+					this.searchText = ''
 				}
 			},
 	}
@@ -33,32 +35,51 @@
 
 <template>
 	<header>
-		<!-- input form per la ricerca dei film -->
-		<div class="input-group mb-3">
-			<!-- input con v-model per collegarlo al data searchText -->
-  			<input v-model="searchText" type="text" class="form-control" placeholder="Cosa vuoi vedere?">
 
-			<!-- bottone da cui ascolto il click per cercare i film/serie tv -->
-  			<button @click="searchMovie()" class="btn btn-outline-secondary" type="submit">
-				<i class="fa-solid fa-magnifying-glass"></i>
-			</button>
+		<div class="container">
+
+			<!-- input form per la ricerca dei film -->
+			<div class="input-group mb-3">
+				<!-- input con v-model per collegarlo al data searchText -->
+				<input v-model="searchText" type="text" class="form-control" placeholder="Cosa vuoi vedere?">
+
+				<!-- bottone da cui ascolto il click per cercare i film/serie tv -->
+				<button @click="searchMovie()" class="btn btn-outline-secondary" type="submit">
+					<i class="fa-solid fa-magnifying-glass"></i>
+				</button>
+			</div>
+
 		</div>
 	</header>
 
 	<main>
-		<div>
-			<div class="card" style="width: 18rem;">
-					<img src="..." class="card-img-top" alt="...">
-				<div class="card-body">
-					<h5 class="card-title">Titolo</h5>
+
+		<div class="container">
+
+			<div class="row">
+
+				<!-- ciclo per ripetere la card con le info dei film -->
+				<div class="col mb-3" v-for="(movie, i) in movies" :key='i'>
+
+					<!-- card in cui compaio le infomrazioni del film -->
+					<div class="card">
+							<img src="..." class="card-img-top" alt="...">
+						<div class="card-body">
+							<h5 class="card-title">Titolo: {{movie.title}}</h5>
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Titolo originale: {{movie.original_title}}</li>
+							<li class="list-group-item">Lingua originale: {{movie.original_language}}</li>
+							<li class="list-group-item">Voto: {{movie.vote_average}}</li>
+						</ul>
+					</div>
+
 				</div>
-				<ul class="list-group list-group-flush">
-					<li class="list-group-item">Titolo Originale</li>
-					<li class="list-group-item">Lingua</li>
-					<li class="list-group-item">Voto</li>
-				</ul>
+
 			</div>
+
 		</div>
+
 	</main>
-	
+
 </template>
